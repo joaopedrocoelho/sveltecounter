e3c8c3965431b51fa71eca17142924e598cee236
Pedro <pedro@webglsoft.com>
2023-01-29 23:37:46 -0300

    :sparkles: added counter and basic styles

---
+++ src/components/Counter.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+</script>
+
+<div
+	class="max-w-sm bg-gray-100
+	shadow-lg m-auto 
+	flex relative 
+	items-center mb-2 py-2"
+>
+	<input class="text-gray-600 mx-4 px-1 w-32" />
+	<span class="text-lg font-bold px-4">1</span>
+	<div class="ml-auto flex">
+		<button class="btn bg-red-500 rounded-l ">+</button>
+		<button class="btn bg-blue-500">-</button>
+		<button class="btn bg-yellow-500 rounded-r">0</button>
+		<button class="bg-white text-gray-600 
+		rounded-full h-8 w-8 mx-4
+		flex items-center justify-center font-bold px-1 py-0">X</button>
+	</div>
+</div>
+
+<style>
+	.btn {
+		--text-opacity: 1;
+		color: #fff;
+		color: rgba(255, 255, 255, var(--text-opacity));
+		padding-top: 0.25rem;
+		padding-bottom: 0.25rem;
+		padding-left: 0.75rem;
+		padding-right: 0.75rem;
+		font-size: 1.125rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Counter from '../components/Counter.svelte';
+</script>
+
+<div class="flex m-auto flex-col max-w-sm">
+	<h1 class="text-6xl text-center mb-6">Multiple Counter</h1>
+	<Counter />
+	<button
+		class="max-w-sm w-full mt-2 m-auto text-center
+ bg-green-400 rounded text-white cursor-pointer"
+	>
+		new counter
+	</button>
+	<p class="flex ">title list:</p>
+	<p class="flex">sum of count:</p>
+</div>
